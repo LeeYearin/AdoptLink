2dee36fdf5347210947443b5a30c83feaf0e1068
LeeYearin <rin0729@dongyang.ac.kr>
2026-05-19 23:28:04 +0900

    feat: add main frame and home page

---
+++ data/animalReq.py
@@ -0,0 +1,26 @@
+import requests
+
+base_url = "http://apis.data.go.kr/1543061/abandonmentPublicService_v2"
+url = base_url + "/abandonmentPublic_v2"
+params = {
+    "serviceKey": "16675a982576de9123a769831c2ca6c0b797733762ae5bd47bfed51688a8f5f1",
+    "numOfRows": 40,
+    "state": "notice",
+    "upkind": "417000",
+    "_type": "json"
+}
+
+res = requests.get(url, params=params)
+data = res.json()
+items = data['response']['body']['items']['item']
+print(items)
+
+from jinja2 import Template
+
+with open("data/template.html", "r", encoding="utf-8") as f:
+    template = Template(f.read())
+
+html = template.render(animals=items)
+
+with open("data/output.html", "w", encoding="utf-8") as f:
+    f.write(html)
